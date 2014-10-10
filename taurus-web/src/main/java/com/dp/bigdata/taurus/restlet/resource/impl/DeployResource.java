14b86c8d9adcd267db52b1569a9c0d99831c09e3
kirin.li <kirin.li@dianping.com>
2014-10-10 17:30:01 +0800

    fix

--- taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/DeployResource.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/DeployResource.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
