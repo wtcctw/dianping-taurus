3ea54886034fb513badf0ba4b8d6a80c04bfcb02
kirin.li <kirin.li@dianping.com>
2015-02-06 18:33:49 +0800

    fix bug

--- taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/HostTaskExecTime.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/HostTaskExecTime.java
@@ -1,5 +1,6 @@
 package com.dp.bigdata.taurus.restlet.resource.impl;
 
+import com.dianping.cat.Cat;
 import com.dp.bigdata.taurus.core.Scheduler;
 import com.dp.bigdata.taurus.generated.mapper.TaskAttemptMapper;
 import com.dp.bigdata.taurus.generated.mapper.TaskMapper;
@@ -24,12 +25,13 @@ public class HostTaskExecTime extends ServerResource implements IHostTaskExecTim
     private TaskAttemptMapper taskAttemptMapper;
     @Autowired
     private TaskMapper taskMapper;
+
     @Override
     public String retrieve() {
         String time = (String) getRequestAttributes().get("time");
         String ip = (String) getRequestAttributes().get("ip");
 
-        time = time.replace("%20"," ");
+        time = time.replace("%20", " ");
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
         Date now;
         String startTime;
@@ -46,26 +48,31 @@ public class HostTaskExecTime extends ServerResource implements IHostTaskExecTim
         ArrayList<TaskAttempt> taskAttempts = taskAttemptMapper.getTaskAttemptsHistory(ip, startTime);
         if (taskAttempts != null && taskAttempts.size() > 0) {
             JsonArray taskExecHistorys = new JsonArray();
-            HashMap<String ,String> taskMap = new HashMap<String, String>();
+            HashMap<String, String> taskMap = new HashMap<String, String>();
 
             for (TaskAttempt taskAttempt : taskAttempts) {
                 String tmp = taskMap.get(taskAttempt.getTaskid());
-                long start = getBufferPos(now,taskAttempt.getStarttime());
-                long end = getBufferPos(now,taskAttempt.getEndtime());
+                long start = getBufferPos(now, taskAttempt.getStarttime());
+                Date tmpDate = taskAttempt.getStarttime();
+                if (tmpDate == null) {
+                    tmpDate = new Date();// 如果为空默认为现在，因为该任务还未执行完成
+                }
+
+                long end = getBufferPos(now, tmpDate);
                 StringBuffer runningHistoryMap = new StringBuffer();
-                for(long i = start; i <= end; i++){
+                for (long i = start; i <= end; i++) {
                     runningHistoryMap.append(i);
                     runningHistoryMap.append("#");
                     runningHistoryMap.append(taskAttempt.getStatus());
-                    if (i != end){
+                    if (i != end) {
                         runningHistoryMap.append(",");
                     }
                 }
-                if(StringUtils.isNotBlank(tmp)){
+                if (StringUtils.isNotBlank(tmp)) {
 
-                    taskMap.put(taskAttempt.getTaskid(),tmp + ","+ runningHistoryMap.toString());
-                }else {
-                    taskMap.put(taskAttempt.getTaskid(),runningHistoryMap.toString());
+                    taskMap.put(taskAttempt.getTaskid(), tmp + "," + runningHistoryMap.toString());
+                } else {
+                    taskMap.put(taskAttempt.getTaskid(), runningHistoryMap.toString());
                 }
 
             }
@@ -90,23 +97,23 @@ public class HostTaskExecTime extends ServerResource implements IHostTaskExecTim
             * */
             Iterator iter = taskMap.entrySet().iterator();
             ArrayList<Task> taskArrayList = taskMapper.getRealTasksByHost(ip);
-            HashMap<String, Task>hostTaskMap = new HashMap<String, Task>();
-            for (Task task: taskArrayList){
+            HashMap<String, Task> hostTaskMap = new HashMap<String, Task>();
+            for (Task task : taskArrayList) {
                 hostTaskMap.put(task.getTaskid(), task);
             }
             while (iter.hasNext()) {
                 Map.Entry entry = (Map.Entry) iter.next();
                 String key = (String) entry.getKey();
-                String  val = (String) entry.getValue();
+                String val = (String) entry.getValue();
 
                 JsonObject taskExecHistory = new JsonObject();
                 Task task = hostTaskMap.get(key);
-                if (task != null){
+                if (task != null) {
                     taskExecHistory.addProperty("taskId", key);
                     taskExecHistory.addProperty("taskName", task.getName());
                     taskExecHistory.addProperty("creator", task.getCreator());
                     taskExecHistory.addProperty("appName", task.getAppname());
-                }else{
+                } else {
                     taskExecHistory.addProperty("taskId", key);
                 }
 
@@ -122,21 +129,22 @@ public class HostTaskExecTime extends ServerResource implements IHostTaskExecTim
     }
 
     long getBufferPos(Date time, Date start) {
-        if (time == null){
-            time = new Date();
+        if (start == null) {
+            start = new Date();
         }
         Long different;
         Date end = new Date(time.getTime() - 24 * 60 * 60 * 1000);
         try {
 
 
-             different = start.getTime() - end.getTime();
+            different = start.getTime() - end.getTime();
             different = different / (1000 * 60);
             return (different / 20 + 1);
-        }catch (Exception e){
-            System.out.println("except===start"+start+"===end:"+end+e.getMessage());
+        } catch (Exception e) {
+            Cat.logError("getBufferPos Error" , e);
+            return 0;
         }
 
-        return 0;
+
     }
 }
