868285a1999205fbc15b178f177f5a4762cc321e
chongze.chen <chongze.chen@dianping.com>
2015-06-25 11:09:07 +0800

    add no sso support

--- taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/LoginController.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/LoginController.java
@@ -18,13 +18,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.CookieGenerator;
 
 import com.dp.bigdata.taurus.generated.module.User;
+import com.dp.bigdata.taurus.restlet.resource.IUserResource;
 import com.dp.bigdata.taurus.restlet.resource.IUsersResource;
 import com.dp.bigdata.taurus.restlet.shared.UserDTO;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 @Controller
-@RequestMapping("/rest")
 public class LoginController {
 	
 	private Logger log = LoggerFactory.getLogger(this.getClass());
@@ -37,7 +41,7 @@ public class LoginController {
 	 * @param encodeRedirectUri
 	 * @throws IOException 
 	 */
-	@RequestMapping(value = "/ssologin", method = RequestMethod.GET)
+	@RequestMapping(value = "/rest/ssologin", method = RequestMethod.GET)
 	public void ssologin(ModelMap modelMap, 
 							HttpServletRequest request,
 							HttpServletResponse response) throws IOException 
@@ -95,7 +99,7 @@ public class LoginController {
 	 * @param response
 	 * @throws IOException
 	 */
-	@RequestMapping(value = "/ssologout", method = RequestMethod.GET)
+	@RequestMapping(value = "/rest/ssologout", method = RequestMethod.GET)
 	public void ssologout(ModelMap modelMap, 
 							HttpServletRequest request,
 							HttpServletResponse response) throws IOException 
@@ -113,6 +117,44 @@ public class LoginController {
         response.sendRedirect(ssoLogoutUrl + "?service=" + URLEncoder.encode(taurusUrl, "UTF-8"));
 	}
 	
+	/**
+	 * sso太慢，调试不方便，留个后门,使用的时候设置sso的excludeURLs为/*，然后用signin页面登录即可
+	 * @param modelMap
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public void oldlogin(ModelMap modelMap, 
+							HttpServletRequest request,
+							HttpServletResponse response) throws IOException 
+	{
+		log.info("--------------init the oldlogin------------");
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (StringUtils.isBlank(password)) {
+			response.setStatus(401);
+			return;
+		}
+		
+		ClientResource cr = new ClientResource(String.format("%s/%s", InitController.RESTLET_URL_BASE + "user", userName));
+		IUserResource resource = cr.wrap(IUserResource.class);
+		boolean hasRegister = resource.hasRegister();
+		if (hasRegister) {
+			HttpSession session = request.getSession();
+			session.setAttribute(InitController.USER_NAME, userName);
+			
+			if(isInfoCompleted(userName)){
+				response.setStatus(200);
+			} else{
+				response.setStatus(201);
+			}
+		} else {
+			response.setStatus(401);
+		}
+		
+	}
 	/**
 	 * 检查用户信息是否完整
 	 * @param userName
