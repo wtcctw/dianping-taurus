b7c46fe004a26825f2cc82ecf1946b4954603456
chongze.chen <chongze.chen@dianping.com>
2015-06-30 19:17:15 +0800

    指定log4j配置，防止sso依赖包吞日志

---
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/web/servlet/MyServletContextListener.java
@@ -0,0 +1,25 @@
+package com.dp.bigdata.taurus.web.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyServletContextListener implements ServletContextListener {
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce)
+	{
+		ServletContext sc = sce.getServletContext();
+		//do your initialization here.
+		//sc.setAttribute(.....);
+		org.apache.log4j.LogManager.resetConfiguration();
+		org.apache.log4j.PropertyConfigurator.configure("src/main/resources/log4j.properties");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce)
+	{
+		ServletContext sc = sce.getServletContext();
+		//do your cleanup here
+	}
+}
