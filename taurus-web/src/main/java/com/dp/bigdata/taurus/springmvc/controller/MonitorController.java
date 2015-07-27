410c1854f4374caeb759093af5b48507abfdb725
chongze.chen <chongze.chen@dianping.com>
2015-07-27 10:51:40 +0800

    尝试java代码格式化

--- taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/MonitorController.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/MonitorController.java
@@ -1,56 +1,57 @@
 package com.dp.bigdata.taurus.springmvc.controller;
 
+import com.dp.bigdata.taurus.restlet.shared.AttemptDTO;
+import com.dp.bigdata.taurus.restlet.shared.TaskDTO;
+import com.dp.bigdata.taurus.zookeeper.execute.helper.ExecuteStatus;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.restlet.resource.ClientResource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.io.IOException;
 import java.io.OutputStream;
+
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.restlet.resource.ClientResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.dp.bigdata.taurus.restlet.shared.AttemptDTO;
-import com.dp.bigdata.taurus.restlet.shared.TaskDTO;
-import com.dp.bigdata.taurus.zookeeper.execute.helper.ExecuteStatus;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 @Controller
 @RequestMapping("/monitor")
 public class MonitorController {
-
-	private Logger log = LoggerFactory.getLogger(this.getClass());
-	
-	//这里要不要考虑同步的问题？
+    //这里要不要考虑同步的问题？
     private static ArrayList<AttemptDTO> attempts;
     private static ArrayList<TaskDTO> tasks;
-    
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/jobdetail", method = RequestMethod.POST)
-	public void jobdetail(ModelMap modelMap, 
-							HttpServletRequest request,
-							HttpServletResponse response) throws IOException 
-    {
-		log.info("--------------init the jobdetail------------");
-        
+    public void jobdetail(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) throws IOException {
+        log.info("--------------init the jobdetail------------");
+
         String start = request.getParameter("start");
         String end = request.getParameter("end");
-
-        ClientResource cr = new ClientResource(InitController.RESTLET_URL_BASE + "jobdetail/" + "/" + start + "/" + end);
+        ClientResource cr = new ClientResource(InitController.RESTLET_URL_BASE +
+                "jobdetail/" + "/" + start + "/" + end);
         String jsonString = cr.get(String.class);
-        
         OutputStream output = response.getOutputStream();
         output.write(jsonString.getBytes());
         output.close();
-	}
-    
+    }
+
     /**
      * 每次加载任务监控monitor.ftl都刷新作业们的运行历史
      * @param modelMap
@@ -59,144 +60,132 @@ public class MonitorController {
      * @throws IOException
      */
     @RequestMapping(value = "/reflash_attempts", method = RequestMethod.POST)
-	public void monitor(ModelMap modelMap, 
-						HttpServletRequest request,
-						HttpServletResponse response) throws IOException 
-    {
-		log.info("--------------init the reflash_attempts------------");
-    	
+    public void monitor(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) throws IOException {
+        log.info("--------------init the reflash_attempts------------");
+
         String taskTime = request.getParameter("start");
         String url = InitController.RESTLET_URL_BASE + "getattemptsbystatus/";
         ClientResource cr = new ClientResource(url + taskTime);
         attempts = cr.get(ArrayList.class);
 
-        cr = new ClientResource(InitController.RESTLET_URL_BASE + "reflashHostLoad");
+        cr = new ClientResource(InitController.RESTLET_URL_BASE +
+                "reflashHostLoad");
         tasks = cr.put(null, ArrayList.class);
-        
+
         if (tasks == null) {
-            ClientResource crTask = new ClientResource(InitController.RESTLET_URL_BASE + "gettasks");
+            ClientResource crTask = new ClientResource(InitController.RESTLET_URL_BASE +
+                    "gettasks");
             tasks = crTask.get(ArrayList.class);
         }
-        
+
         OutputStream output = response.getOutputStream();
         output.write("success".getBytes());
         output.close();
-	}
-	
+    }
+
     @RequestMapping(value = "/runningtasks", method = RequestMethod.GET)
-	public String runningtasks(ModelMap modelMap, 
-								HttpServletRequest request,
-								HttpServletResponse response) 
-    {
-		log.info("--------------init the runningtasks------------");
-    	
+    public String runningtasks(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) {
+        log.info("--------------init the runningtasks------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
         modelMap.addAttribute("mHelper", new MonitorHelper());
-        
+
         return "/monitor/runningtasks.ftl";
-	}
-    
-    
+    }
+
     @RequestMapping(value = "/submitfail", method = RequestMethod.POST)
-	public String submitfail(ModelMap modelMap, 
-								HttpServletRequest request,
-								HttpServletResponse response) 
-    {
-		log.info("--------------init the submitfail------------");
-    	
+    public String submitfail(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) {
+        log.info("--------------init the submitfail------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
         modelMap.addAttribute("mHelper", new MonitorHelper());
-        
+
         return "/monitor/submitfail.ftl";
-	}
-    
+    }
+
     @RequestMapping(value = "/dependencypass", method = RequestMethod.POST)
-	public String dependencypass(ModelMap modelMap, 
-									HttpServletRequest request,
-									HttpServletResponse response) 
-    {
-		log.info("--------------init the dependencypass------------");
-    	
+    public String dependencypass(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) {
+        log.info("--------------init the dependencypass------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
-        
+
         return "/monitor/dependencypass.ftl";
-	}
-    
+    }
+
     @RequestMapping(value = "/failedtasks", method = RequestMethod.POST)
-	public String failedtasks(ModelMap modelMap, 
-								HttpServletRequest request,
-								HttpServletResponse response) 
-    {
-		log.info("--------------init the failedtasks------------");
-    	
+    public String failedtasks(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) {
+        log.info("--------------init the failedtasks------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
         modelMap.addAttribute("mHelper", new MonitorHelper());
-        
+
         return "/monitor/failedtasks.ftl";
-	}
-    
+    }
+
     @RequestMapping(value = "/dependencytimeout", method = RequestMethod.POST)
-	public String dependencytimeout(ModelMap modelMap, 
-									HttpServletRequest request,
-									HttpServletResponse response) 
-    {
-		log.info("--------------init the dependencytimeout------------");
-    	
+    public String dependencytimeout(ModelMap modelMap,
+        HttpServletRequest request, HttpServletResponse response) {
+        log.info("--------------init the dependencytimeout------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
         modelMap.addAttribute("mHelper", new MonitorHelper());
-        
+
         return "/monitor/dependencytimeout.ftl";
-	}
-    
+    }
+
     @RequestMapping(value = "/timeout", method = RequestMethod.POST)
-	public String timeout(ModelMap modelMap, 
-							HttpServletRequest request,
-							HttpServletResponse response) 
-    {
-		log.info("--------------init the timeout------------");
-    	
+    public String timeout(ModelMap modelMap, HttpServletRequest request,
+        HttpServletResponse response) {
+        log.info("--------------init the timeout------------");
+
         modelMap.addAttribute("attempts", attempts);
         modelMap.addAttribute("tasks", tasks);
         modelMap.addAttribute("mHelper", new MonitorHelper());
-        
+
         return "/monitor/timeout.ftl";
-	}
-    
-    public class MonitorHelper{
-    	
-    	/**
-    	 * runningtasks.ftl辅助方法
-    	 * @param ip
-    	 * @return
-    	 */
-    	public boolean isViewLog(String ip){
-    		boolean result = false;
+    }
+
+    public class MonitorHelper {
+        /**
+             * runningtasks.ftl辅助方法
+             * @param ip
+             * @return
+             */
+        public boolean isViewLog(String ip) {
+            boolean result = false;
 
             if (InitController.ZABBIX_SWITCH.equals("false")) {
-            	result = false;
+                result = false;
             } else {
-            	result = AttemptProxyController.isHostOverLoad(ip);
+                result = AttemptProxyController.isHostOverLoad(ip);
             }
-            
+
             return result;
-    	}
-    	
-    	/**
-    	 * submitfail.ftl等辅助方法
-    	 * @param taskID
-    	 * @return
-    	 */
-    	public String getLastTaskStatus(String taskID){
-    		String status_api = InitController.RESTLET_URL_BASE + "getlaststatus";
+        }
+
+        /**
+             * submitfail.ftl等辅助方法
+             * @param taskID
+             * @return
+             */
+        public String getLastTaskStatus(String taskID) {
+            String status_api = InitController.RESTLET_URL_BASE +
+                "getlaststatus";
             String status = null;
-            
+
             try {
-            	ClientResource cr = new ClientResource(status_api + "/" + taskID);
+                ClientResource cr = new ClientResource(status_api + "/" +
+                        taskID);
                 status = cr.get(String.class);
             } catch (Exception e) {
                 status = null;
@@ -204,7 +193,7 @@ public class MonitorController {
 
             String lastTaskStatus = null;
             int taskState = -1;
-            
+
             if (status != null) {
                 try {
                     JsonParser parser = new JsonParser();
@@ -219,8 +208,8 @@ public class MonitorController {
                     lastTaskStatus = "NULL";
                 }
             }
-            
+
             return lastTaskStatus;
-    	}
+        }
     }
 }
