d5d0df900a918e19c46e7676bf1f132558b21a7b
kirin.li <kirin.li@dianping.com>
2015-08-07 13:59:16 +0800

    fix bug && add env

--- taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/RunningAttemptsResource.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/RunningAttemptsResource.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import com.dianping.cat.Cat;
+import com.dianping.lion.Environment;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import groovy.json.JsonException;
@@ -28,27 +30,39 @@ public class RunningAttemptsResource extends ServerResource implements
 	public String retrieve() {
 		String taskID = (String) getRequest().getAttributes().get("taskID");
 		List<AttemptContext> runnings = scheduler.getRunningAttemptsByTaskID(taskID);
-        JsonArray jsonArray = new JsonArray();
         Date time = null;
-        for(AttemptContext running: runnings){
 
-            if (time == null || time.before(running.getScheduletime())){
-                time = running.getScheduletime();
+        if (runnings != null){
+            for(AttemptContext running: runnings){
+
+                if (time == null || time.before(running.getScheduletime())){
+                    time = running.getScheduletime();
+                }
+
             }
 
 
-
         }
+        
         JsonObject scheduleTimeJson = new JsonObject();
+
         try {
-            scheduleTimeJson.addProperty("last-schedule-time", time.getTime());
+            if (time != null) {
+                scheduleTimeJson.addProperty("last-schedule-time", time.getTime());
+            }else{
+                scheduleTimeJson.addProperty("last-schedule-time", -1);
+            }
+
+            String env = Environment.getEnv();
+            scheduleTimeJson.addProperty("env", env);
         } catch (JsonException e) {
 
             Cat.logError("Transfer Json Error", e);
 
             e.printStackTrace();
         }
-		return scheduleTimeJson.toString();
-	}
+        return scheduleTimeJson.toString();
+
+    }
 
 }
