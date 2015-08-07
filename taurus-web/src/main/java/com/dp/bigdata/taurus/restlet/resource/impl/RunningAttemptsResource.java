b77262886f98c839e899e659b6d0499742cb71c2
kirin.li <kirin.li@dianping.com>
2015-08-07 13:39:46 +0800

    fix last schedule time api

--- taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/RunningAttemptsResource.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/restlet/resource/impl/RunningAttemptsResource.java
@@ -1,9 +1,16 @@
 package com.dp.bigdata.taurus.restlet.resource.impl;
 
+import java.util.Date;
 import java.util.List;
 
+import com.dianping.cat.Cat;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import groovy.json.JsonException;
 import net.sf.json.JSONArray;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.restlet.resource.ServerResource;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,8 +28,27 @@ public class RunningAttemptsResource extends ServerResource implements
 	public String retrieve() {
 		String taskID = (String) getRequest().getAttributes().get("taskID");
 		List<AttemptContext> runnings = scheduler.getRunningAttemptsByTaskID(taskID);
-		JSONArray jsonArr = JSONArray.fromObject(runnings);
-		return jsonArr.toString();
+        JsonArray jsonArray = new JsonArray();
+        Date time = null;
+        for(AttemptContext running: runnings){
+
+            if (time == null || time.before(running.getScheduletime())){
+                time = running.getScheduletime();
+            }
+
+
+
+        }
+        JsonObject scheduleTimeJson = new JsonObject();
+        try {
+            scheduleTimeJson.addProperty("last-schedule-time", time.getTime());
+        } catch (JsonException e) {
+
+            Cat.logError("Transfer Json Error", e);
+
+            e.printStackTrace();
+        }
+		return scheduleTimeJson.toString();
 	}
 
 }
--- taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/UserController.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/UserController.java
@@ -82,4 +82,6 @@ public class UserController {
 			return name;
 		}
 	}
+
+
 }
