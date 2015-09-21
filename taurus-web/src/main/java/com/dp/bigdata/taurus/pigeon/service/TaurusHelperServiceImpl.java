35639ff3470c99c6bd59c40a53640e80bd5782a2
chongze.chen <chongze.chen@dianping.com>
2015-09-21 19:26:26 +0800

    impl pigeon service

--- taurus-web/src/main/java/com/dp/bigdata/taurus/pigeon/service/TaurusHelperServiceImpl.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/pigeon/service/TaurusHelperServiceImpl.java
@@ -1,5 +1,26 @@
 package com.dp.bigdata.taurus.pigeon.service;
 
-public class TaurusHelperServiceImpl {
+import java.util.ArrayList;
+
+import org.restlet.resource.ClientResource;
+
+import com.dianping.pigeon.remoting.provider.config.annotation.Service;
+import com.dianping.taurus.service.TaurusHelperService;
+import com.dp.bigdata.taurus.restlet.shared.AttemptDTO;
+import com.dp.bigdata.taurus.restlet.utils.LionConfigUtil;
+
+@Service
+public class TaurusHelperServiceImpl implements TaurusHelperService{
+
+	@Override
+	public String getTaurusAttemptInfoByTaskID(String taskID) {
+		
+		String url = LionConfigUtil.RESTLET_API_BASE + "attempt?task_id=" + taskID;
+		ClientResource cr = new ClientResource(url);
+        cr.setRequestEntityBuffering(true);
+        ArrayList<AttemptDTO> attempts = cr.get(ArrayList.class);
+		return attempts.toString();
+		
+	}
 
 }
