ad89b0595c0bada8328f8455e7e3bba80d498e56
lmdyyh <lmdyyh@gmail.com>
2016-03-08 15:48:57 +0800

    增加跳过sso开关

--- taurus-web/src/main/java/com/dp/bigdata/taurus/web/servlet/filter/AuthenticationFilter.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/web/servlet/filter/AuthenticationFilter.java
@@ -1,90 +1,90 @@
 package com.dp.bigdata.taurus.web.servlet.filter;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import com.dp.bigdata.taurus.restlet.shared.UserDTO;
+import com.dp.bigdata.taurus.restlet.utils.LionConfigUtil;
+import com.dp.bigdata.taurus.springmvc.controller.InitController;
 import org.apache.commons.lang.StringUtils;
 import org.restlet.resource.ClientResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dp.bigdata.taurus.restlet.shared.UserDTO;
-import com.dp.bigdata.taurus.restlet.utils.LionConfigUtil;
-import com.dp.bigdata.taurus.springmvc.controller.InitController;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * AuthenticationFilter
- * 
+ *
  * @author damon.zhu
  */
 public class AuthenticationFilter implements Filter {
-	
-	private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-	      ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;
-		String userInfoStr = req.getRemoteUser();
-		
-		String dpaccount = null;
-		try {
-			dpaccount = userInfoStr.split("\\|")[0];
-		} catch (Exception e) {
-			log.error("get remote user error!");
-		}
-		
-		HttpSession session = req.getSession(true);
-		String sessionAccount = (String) session.getAttribute(InitController.USER_NAME);
-		
-		if(StringUtils.isNotBlank(sessionAccount)){
-			log.info(sessionAccount + " already logged in.");
-			chain.doFilter(request, response);
-			return;
-		}
-		
-		if(StringUtils.isBlank(sessionAccount)) {
-			
-			if(StringUtils.isBlank(dpaccount)){
-				res.sendRedirect(req.getContextPath() + "/error");
-				return ;
-			}
-			
-			UserDTO userDTO = setUserInfo(dpaccount);
-			session.setAttribute(InitController.USER_NAME, dpaccount);
-			
-			ClientResource cr = new ClientResource(LionConfigUtil.RESTLET_API_BASE + "user");
-			cr.post(userDTO);//createIfNotExist
-			
-			chain.doFilter(request, response);
-			return;
-		}
-		
-	}
+    private String userNameUnderSsoDisable;
 
-	@Override
-	public void destroy() {
-	}
-	
-	private UserDTO setUserInfo(String userName){
-		UserDTO userDTO = new UserDTO();
-		userDTO.setName(userName);
-		userDTO.setMail(userName+"@dianping.com");
-		return userDTO;
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        userNameUnderSsoDisable = filterConfig.getInitParameter("userName");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String userInfoStr = req.getRemoteUser();
+
+        String dpaccount = null;
+        try {
+            if (StringUtils.isBlank(userInfoStr)) {
+                dpaccount = userNameUnderSsoDisable;
+            } else {
+                dpaccount = userInfoStr.split("\\|")[0];
+            }
+        } catch (Exception e) {
+            log.error("get remote user error!");
+        }
+
+        HttpSession session = req.getSession(true);
+        String sessionAccount = (String) session.getAttribute(InitController.USER_NAME);
+
+        if (StringUtils.isNotBlank(sessionAccount)) {
+            log.info(sessionAccount + " already logged in.");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        if (StringUtils.isBlank(sessionAccount)) {
+
+            if (StringUtils.isBlank(dpaccount)) {
+                res.sendRedirect(req.getContextPath() + "/error");
+                return;
+            }
+
+            UserDTO userDTO = setUserInfo(dpaccount);
+            session.setAttribute(InitController.USER_NAME, dpaccount);
+
+            ClientResource cr = new ClientResource(LionConfigUtil.RESTLET_API_BASE + "user");
+            cr.post(userDTO);//createIfNotExist
+
+            chain.doFilter(request, response);
+            return;
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    private UserDTO setUserInfo(String userName) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName(userName);
+        userDTO.setMail(userName + "@dianping.com");
+        return userDTO;
+    }
 
 }
---
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/web/servlet/filter/decorator/SingleSignOutFilterDecorator.java
@@ -0,0 +1,52 @@
+package com.dp.bigdata.taurus.web.servlet.filter.decorator;
+
+import org.jasig.cas.client.session.SingleSignOutFilter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Author   mingdongli
+ * 15/12/2  下午8:22.
+ */
+public class SingleSignOutFilterDecorator extends SSOSwitchFilter implements Filter {
+
+    private SingleSignOutFilter singleSignOutFilter;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        super.doFilter(request, response, chain);
+    }
+
+    @Override
+    public void destroy() {
+
+        super.destroy();
+    }
+
+    @Override
+    protected void internalDoInit(FilterConfig filterConfig) throws ServletException {
+
+        singleSignOutFilter = new SingleSignOutFilter();
+        singleSignOutFilter.init(filterConfig);
+    }
+
+    @Override
+    protected void internalDoFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        singleSignOutFilter.doFilter(request, response, chain);
+    }
+
+    @Override
+    protected void internalDoDestroy() {
+
+        singleSignOutFilter.destroy();
+    }
+}
