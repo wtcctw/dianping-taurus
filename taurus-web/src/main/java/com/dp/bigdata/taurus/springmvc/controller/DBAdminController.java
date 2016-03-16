403317d794607b8bcdca217c5718b9c7b90c0020
lmdyyh <lmdyyh@gmail.com>
2016-03-16 17:50:35 +0800

    注释切换master的api

--- taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/DBAdminController.java
+++ taurus-web/src/main/java/com/dp/bigdata/taurus/springmvc/controller/DBAdminController.java
@@ -1,12 +1,10 @@
 package com.dp.bigdata.taurus.springmvc.controller;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.dp.bigdata.taurus.alert.TaurusAlert;
+import com.dp.bigdata.taurus.core.AttemptStatusMonitor;
+import com.dp.bigdata.taurus.core.Engine;
+import com.dp.bigdata.taurus.restlet.utils.LionConfigUtil;
+import com.dp.bigdata.taurus.zookeeper.common.infochannel.ZooKeeperCleaner;
 import org.restlet.resource.ClientResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,18 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dp.bigdata.taurus.alert.TaurusAlert;
-import com.dp.bigdata.taurus.core.AttemptStatusMonitor;
-import com.dp.bigdata.taurus.core.Engine;
-import com.dp.bigdata.taurus.restlet.utils.ClearLogsTimerManager;
-import com.dp.bigdata.taurus.restlet.utils.LionConfigUtil;
-import com.dp.bigdata.taurus.restlet.utils.MonitorAgentOffLineTaskTimer;
-import com.dp.bigdata.taurus.restlet.utils.ReFlashHostLoadTaskTimer;
-import com.dp.bigdata.taurus.springmvc.bean.WebResult;
-import com.dp.bigdata.taurus.zookeeper.common.infochannel.ZooKeeperCleaner;
-import com.dp.bigdata.taurus.zookeeper.common.utils.IPUtils;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Controller
 public class DBAdminController {
@@ -47,73 +39,73 @@ public class DBAdminController {
     @Autowired
     public AttemptStatusMonitor statusMonitor;
 	
-    @RequestMapping(value = "/dbadmin/changeServer", method = RequestMethod.POST)
-    @ResponseBody
-    public WebResult changeServer(HttpServletRequest request, HttpServletResponse response) throws InterruptedException{
-    	log.info("--------------init the dbadmin/changeServer------------");
-    	
-    	WebResult result = new WebResult(request);
-    	String adminUser = InitController.ADMIN_USER;
-    	String user = (String) request.getSession().getAttribute("taurus-user");
-    	
-    	if (adminUser.contains(user)){
-    		
-        	if(LionConfigUtil.loadServerConf()){
-        		
-        		//设置为master server并启动调度
-            	if(LionConfigUtil.SERVER_MASTER_IP.equals(IPUtils.getFirstNoLoopbackIP4Address())){
-            		engine.load();
-            		alert.load();
-            		alert.isInterrupt(false);
-            		engine.isInterrupt(false);
-            		
-            		if(ClearLogsTimerManager.getClearLogsTimerManager().getTimer() == null){
-            			ClearLogsTimerManager.getClearLogsTimerManager().start();
-            		}
-            		
-            		if(MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().getTimer() == null){
-            			MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().start();
-            		}
-            		
-            		if(ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().getTimer() == null){
-            			ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().start();
-            		}
-            		
-            		result.setMessage("Set localhost(" + LionConfigUtil.SERVER_MASTER_IP + ") master server");
-            	}else{//关闭调度并指向master server的restlet
-            		alert.isInterrupt(true);
-            		engine.isInterrupt(true);
-            		
-            		if(ClearLogsTimerManager.getClearLogsTimerManager().getTimer() != null){
-            			ClearLogsTimerManager.getClearLogsTimerManager().stop();
-            		}
-            		
-            		if(MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().getTimer() != null){
-            			MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().stop();
-            		}
-            		
-            		if(ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().getTimer() != null){
-            			ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().stop();
-            		}
-            		
-            		while(!(engine.isTriggleThreadRestFlag()
-            				&& statusMonitor.isAttemptStatusMonitorRestFlag())){
-            			//wait the engine to finish last schedule
-            		}
-            		
-            		result.setMessage("Change master server to " + LionConfigUtil.SERVER_MASTER_IP);
-            	}
-            	
-        	}else{
-        		result.setMessage("Error!!cannot read from lion server.");
-        	}
-        	
-    	}else{
-    		result.setMessage("You are not admin user.");
-    	}
-    	
-		return result;
-    }
+//    @RequestMapping(value = "/dbadmin/changeServer", method = RequestMethod.POST)
+//    @ResponseBody
+//    public WebResult changeServer(HttpServletRequest request, HttpServletResponse response) throws InterruptedException{
+//    	log.info("--------------init the dbadmin/changeServer------------");
+//
+//    	WebResult result = new WebResult(request);
+//    	String adminUser = InitController.ADMIN_USER;
+//    	String user = (String) request.getSession().getAttribute("taurus-user");
+//
+//    	if (adminUser.contains(user)){
+//
+//        	if(LionConfigUtil.loadServerConf()){
+//
+//        		//设置为master server并启动调度
+//            	if(LionConfigUtil.SERVER_MASTER_IP.equals(IPUtils.getFirstNoLoopbackIP4Address())){
+//            		engine.load();
+//            		alert.load();
+//            		alert.isInterrupt(false);
+//            		engine.isInterrupt(false);
+//
+//            		if(ClearLogsTimerManager.getClearLogsTimerManager().getTimer() == null){
+//            			ClearLogsTimerManager.getClearLogsTimerManager().start();
+//            		}
+//
+//            		if(MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().getTimer() == null){
+//            			MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().start();
+//            		}
+//
+//            		if(ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().getTimer() == null){
+//            			ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().start();
+//            		}
+//
+//            		result.setMessage("Set localhost(" + LionConfigUtil.SERVER_MASTER_IP + ") master server");
+//            	}else{//关闭调度并指向master server的restlet
+//            		alert.isInterrupt(true);
+//            		engine.isInterrupt(true);
+//
+//            		if(ClearLogsTimerManager.getClearLogsTimerManager().getTimer() != null){
+//            			ClearLogsTimerManager.getClearLogsTimerManager().stop();
+//            		}
+//
+//            		if(MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().getTimer() != null){
+//            			MonitorAgentOffLineTaskTimer.getMonitorAgentOffLineTimeManager().stop();
+//            		}
+//
+//            		if(ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().getTimer() != null){
+//            			ReFlashHostLoadTaskTimer.getReFlashHostLoadManager().stop();
+//            		}
+//
+//            		while(!(engine.isTriggleThreadRestFlag()
+//            				&& statusMonitor.isAttemptStatusMonitorRestFlag())){
+//            			//wait the engine to finish last schedule
+//            		}
+//
+//            		result.setMessage("Change master server to " + LionConfigUtil.SERVER_MASTER_IP);
+//            	}
+//
+//        	}else{
+//        		result.setMessage("Error!!cannot read from lion server.");
+//        	}
+//
+//    	}else{
+//    		result.setMessage("You are not admin user.");
+//    	}
+//
+//		return result;
+//    }
     
     
 	@RequestMapping(value = "/db_admin.do", method = RequestMethod.POST)
