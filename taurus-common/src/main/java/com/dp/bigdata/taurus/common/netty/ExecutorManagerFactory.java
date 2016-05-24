88e6eab59c372c34f9ff449405bdfb95092491af
lmdyyh <lmdyyh@gmail.com>
2016-05-24 19:33:13 +0800

    代理抛出ExecuteException异常

--- taurus-common/src/main/java/com/dp/bigdata/taurus/common/netty/ExecutorManagerFactory.java
+++ taurus-common/src/main/java/com/dp/bigdata/taurus/common/netty/ExecutorManagerFactory.java
@@ -1,6 +1,7 @@
 package com.dp.bigdata.taurus.common.netty;
 
 import com.dp.bigdata.taurus.common.execute.ExecuteContext;
+import com.dp.bigdata.taurus.common.execute.ExecuteException;
 import com.dp.bigdata.taurus.common.execute.ExecutorManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -54,22 +55,27 @@ public class ExecutorManagerFactory implements FactoryBean<ExecutorManager>, Inv
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-        if(args.length == 1){
-            ExecuteContext executeContext = (ExecuteContext) args[0];
-            String type = executeContext.getType();
-            if(MscheduleExecutorManager.MSCHEDULE_TYPE.equalsIgnoreCase(type)){
-                return method.invoke(netty, args);
-            }else {
-                return method.invoke(zookeeper, args);
-            }
+        try {
+            if(args.length == 1){
+                ExecuteContext executeContext = (ExecuteContext) args[0];
+                String type = executeContext.getType();
+                if(MscheduleExecutorManager.MSCHEDULE_TYPE.equalsIgnoreCase(type)){
+                    return method.invoke(netty, args);
+                }else {
+                    return method.invoke(zookeeper, args);
+                }
 
-        }else if(args.length == 2){
-            logger.info(String.format("invoke %s", method.toString()));
-            return method.invoke(zookeeper, args);
-        }else {
-            logger.info(String.format("invoke %s error", method.toString()));
-            return null;
+            }else if(args.length == 2){
+                logger.info(String.format("invoke %s", method.toString()));
+                return method.invoke(zookeeper, args);
+            }else {
+                logger.info(String.format("invoke %s error", method.toString()));
+                return null;
+            }
+        }catch (Throwable t){
+            throw new ExecuteException(t);
         }
+
     }
 
     public ExecutorManager getNetty() {
